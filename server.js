0ccd8f3c30fa5aaa0cc2b617722b132e3d8594b1
Gaetan LAVIT <g.lavit@outlook.fr>
2022-08-22 09:00:20 +0400

    Définition du moteur de vues + assets

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+
 var corsOptions = {
     origin: "http://localhost:7070"
 };
-
-  app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
 app.use(express.json());
@@ -13,9 +13,19 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
+//Set view engine
+app.set('view engine', 'ejs')
+
+// Static Files
+app.use(express.static('public'));
+
 app.get('/',function(req,res){
     res.json({message : "Server running"});
   });
+
+  app.get('/ejs', (req, res) => {
+    res.render('pages/index', {title : "Welcome"});
+  })
 
 const PORT = process.env.PORT || 7070
 
