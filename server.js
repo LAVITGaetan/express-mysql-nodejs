b340349a64102cf17ecb0b58574f2e2f59d0b13e
Gaetan LAVIT <g.lavit@outlook.fr>
2022-09-01 11:00:51 +0400

    Amélioration UI /UX

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const session = require('express-session');
-const services = require('./app/services/render.js')
+const services = require('./app/services/render.js');
+const fileUpload = require('express-fileupload');
 
 var corsOptions = {
   origin: "http://localhost:7070"
@@ -20,6 +21,13 @@ app.set('view engine', 'ejs')
 
 // Static Files
 app.use(express.static('public'));
+
+app.use(fileUpload({
+  limits: {
+      fileSize: 10000000, // Around 10MB
+  },
+  abortOnLimit: true,
+}))
 
 // Session
 
@@ -59,6 +67,17 @@ app.get('/edit-portrait', services.editPortrait)
 // Annuaires
 app.get('/edit-annuaire', services.editAnnuaire)
 
+//upload
+app.post('/upload', (req, res) => {
+    const { image } = req.files;
+
+    // If no image submitted, exit
+    if (!image) return res.sendStatus(400);
+
+    // Ajouter l' image au dossier "upload"
+    image.mv(__dirname + '/upload/' + image.name);
+});
+
 const PORT = process.env.PORT || 7070
 
 require("./app/routes/adherent.routes.js")(app);
@@ -67,5 +86,5 @@ require("./app/routes/mandat.routes.js")(app);
 require("./app/routes/portrait.routes.js")(app);
 require("./app/routes/representation.routes.js")(app)
 app.listen(PORT, () => {
-  console.log(`Serveur actif à l'adresse http://localhost:${PORT}`);
+  console.log(`Serveur actif => http://localhost:${PORT}`);
 })
